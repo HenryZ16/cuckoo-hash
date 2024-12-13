e004c76f742f0dd51cebf2a8bc2ef5e8c5e3fcb9
HenryZ16 <1546169856@qq.com>
2024-12-13 19:21:52 +0800

    init_1

--- config.cpp
+++ config.cpp
@@ -1,9 +1,9 @@
 #include <fstream>
 
 /* configs:
- *   num_hash_func <int>      # The number of hash functions used in Cuckoo Hash.
- *   size_hash_table
- *   input_file   <filename> # The input file used to benchmark. If <filename> is stdin, then receive commands from console.
+ *   num_hash_func   <int>    # The number of hash functions used in Cuckoo Hash.
+ *   size_hash_table <int>    # 
+ *   input_file      <string> # The input file used to benchmark. If <filename> is stdin, then receive commands from console.
  */
 
 int 
