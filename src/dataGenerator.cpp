2381e95ef71bc728897437cf4f19f392116dc48f
HenryZ16 <1546169856@qq.com>
2024-12-18 21:06:01 +0800

    fix

--- src/dataGenerator.cpp
+++ src/dataGenerator.cpp
@@ -1,8 +1,14 @@
 #include <cstdint>
 #include <fstream>
+#include <memory>
 #include <random>
 
 #include "utils.h"
+
+// generate insert instruction for 16777216 keys, then generate lookup
+// instruction 11 times for the keys. For the i-th lookup, 100 - 10i keys are
+// randomly chosen from the 16777216 keys, and the rest are random keys.
+int gen_exp2(std::ofstream &output_file);
 
 // Generate a file with the format:
 // <instruction:2> <cnt:30> <key:32> ... <key:32>
@@ -21,11 +27,18 @@ int main(int argc, char **argv) {
     uint32_t cnt = std::stoi(argv[i + 1]);
     if (!output_file.is_open()) {
       println("Failed to open file: {}", argv[1]);
+      output_file.close();
       return 1;
+    }
+    if (instruction == "exp2") {
+      auto ret = gen_exp2(output_file);
+      output_file.close();
+      return ret;
     }
     if (instruction != "insert" && instruction != "delete" &&
         instruction != "lookup") {
       println("Invalid instruction: {}", instruction);
+      output_file.close();
       return 1;
     }
 
@@ -48,5 +61,41 @@ int main(int argc, char **argv) {
   }
 
   output_file.close();
+  return 0;
+}
+
+int gen_exp2(std::ofstream &output_file) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<uint32_t> dis(1, UINT32_MAX);
+
+  uint32_t instruction_code = 1;
+  uint32_t cnt = 16777216;
+  auto S = std::unique_ptr<uint32_t[]>(new uint32_t[cnt]);
+  uint32_t header = (instruction_code << 30) | cnt;
+  output_file.write(reinterpret_cast<char *>(&header), sizeof(header));
+  for (uint32_t i = 0; i < cnt; ++i) {
+    uint32_t key = dis(gen);
+    S[i] = key;
+    output_file.write(reinterpret_cast<char *>(&key), sizeof(key));
+  }
+  std::cout << "Generated 16777216 keys\n";
+
+  std::uniform_int_distribution<uint32_t> dis_S(0, cnt - 1);
+  for (int i = 0; i < 11; ++i) {
+    auto S_cnt = (100 - 10 * i) * cnt / 100;
+    header = (2 << 30) | cnt;
+    output_file.write(reinterpret_cast<char *>(&header), sizeof(header));
+    for (uint32_t i = 0; i < S_cnt; ++i) {
+      uint32_t key = S[dis_S(gen)];
+      output_file.write(reinterpret_cast<char *>(&key), sizeof(key));
+    }
+    for (uint32_t i = 0; i < cnt - S_cnt; ++i) {
+      uint32_t key = dis(gen);
+      output_file.write(reinterpret_cast<char *>(&key), sizeof(key));
+    }
+    std::cout << "Generated " << i << "-th lookup\n";
+  }
+
   return 0;
 }
